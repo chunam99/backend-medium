c9d4f72c6b2e76fdeb5c6fa62fdd3ee40334f21a
Nam Chu <chunam47@gmail.com>
2024-11-11 16:33:42 +0700

    [UPDATE]: add CORS

--- configs/cors.go
+++ configs/cors.go
@@ -8,9 +8,10 @@ import (
 
 func SetupCORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		c.Header("Access-Control-Allow-Credentials", "true")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusOK)
 			return
